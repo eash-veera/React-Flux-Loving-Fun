a66cb39128c605eba31f10c253e71b01546ee8b2
Eshwaran Veerabahu <eshwaranveerabahu.biz@gmail.com>
2017-05-02 11:50:24 +0530

    session 3 done

--- Public/app.jsx
+++ Public/app.jsx
@@ -1,3 +1,55 @@
+//Deviding up single component into multiple atomic components which are nested, these nested components will later be wired.
+
+//Atomic child components
+var ReactGreeterMessageComponent = React.createClass({
+  render: function(){
+    var name = this.props.name;
+    var description = this.props.description;
+
+    return(
+      <div id="reactGreeterMessageComponent">
+        <h1>{name}</h1>
+        <p>{description}</p>
+      </div>
+    );
+  }
+});
+
+var ReactGreeterFormComponent = React.createClass({
+  onFormSubmit: function(e){
+    e.preventDefault();
+
+    var updates = {
+      title : this.refs.name.value,
+      description : this.refs.description.value
+    };
+
+    if(updates.title.length || updates.description.length > 0){
+      this.refs.name.value = '';
+      this.refs.description.value = '';
+      this.props.onNewData(updates);
+    }else{
+      console.log('Please try not to leave your feilds empty.')
+    }
+  },
+  render: function(){
+    return(
+      <form name="simpleNameForm" onSubmit={this.onFormSubmit}>
+        <div>
+          <input placeholder="Insert name here !" type="text" ref="name"/>
+        </div>
+        <div>
+          <textarea placeholder="Insert description about you !" type="text" ref="description"/>
+        </div>
+        <div>
+          <button>Set Name</button>
+        </div>
+      </form>
+    );
+  }
+});
+
+//Parent component which will have the above atomic components nested within
 var ReactGreeterComponent = React.createClass({
   //getDefaultProps allows our component to render default values when the props return empty.
   getDefaultProps: function(){
@@ -6,41 +58,44 @@ var ReactGreeterComponent = React.createClass({
       message : 'This is a default message from the react component props.'
     };
   },
-  onNameSubmit: function(e){
-    //to prevent default behavior of the browser from refreshing on form submit
-    e.preventDefault();
+  getInitialState: function(){
 
-    var formName = this.refs.name.value;
-
-    alert(formName);
+    //initialize the state object so that it can be mutated by initial data.
+    //this can either be from refs (user interaction) or setting data variables.
+    return {
+      name: this.props.name,
+      description: this.props.description
+    };
+  },
+  handleNewData: function(updates){
+    this.setState({
+      name: updates.title,
+      description : updates.description
+    });
   },
   render : function(){
     // we are telling our component what props we need to look for.
-    var name = this.props.name,
-        message = this.props.message;
+    var name = this.state.name,
+        message = this.state.description;
 
     // this is where we return the jsx code that defines the dom elements in our component.
     // binding variables to our static content is done through {} syntax
     return(
-      <div id="reactGreeterComponent">
-        <h1>Hello {name} !</h1>
-        <p>{message}</p>
-
-        <form name="simpleNameForm" onSubmit={this.onNameSubmit}>
-          <input placeholder="Insert name here !" type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
+      <div>
+        <ReactGreeterMessageComponent name={name} description={mess
+            age}/>
+        <ReactGreeterFormComponent onNewData={this.handleNewData}/>
       </div>
     );
   }
 });
 
-var userName = 'Eshwaran Veerabahu',
-    userMessage = 'Hi guys Im '+userName+', I love to learn react.';
+var userDefaultName = '*Add name',
+    userDefaultDescription = '*Add description';
 
 // we use RecatDOM.render() takes in two arguments, *the component we want to render and *which area we need to render.
 
 ReactDOM.render(
-  <ReactGreeterComponent name={userName} message={userMessage}/>,
+  <ReactGreeterComponent name={userDefaultName} description={userDefaultDescription}/>,
   document.getElementById('app')
 );
